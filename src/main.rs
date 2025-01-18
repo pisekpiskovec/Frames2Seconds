f79875d9010b218923c431a7cd8aa6d0cdca8a6c
Písek Pískovec <77384152+pisekpiskovec@users.noreply.github.com>
2025-01-18 13:44:17 +0100

    First sucessful calculation

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
+use regex::Regex;
 use text_io::read;
 
 fn max_fps_settings() -> f32 {
     print!("Enter file's FPS: ");
-    let ret: f32 = read!();
-    ret
+    read!()
+}
+
+fn calculate_miliseconds(max_dot_seconds: u32, max_fps: f32, frame_of_second: u32) -> u32 {
+    ((max_dot_seconds * frame_of_second) as f32 / max_fps) as u32
 }
 
 fn main() {
-    let max_fps: f32 = max_fps_settings();
-    // let cin: String = read!();
-    println!("{}", max_fps);
-    read!();
+    const MAX_DOT_SECONDS: u32 = 999999999;
+
+    let regexp = Regex::new(r"[\d]+$").unwrap();
+    let mut max_fps: f32 = max_fps_settings();
+    loop {
+        print!("Enter frame number: ");
+        let cin: String = read!();
+
+        if regexp.is_match(&cin) {
+            let result =
+                calculate_miliseconds(MAX_DOT_SECONDS, max_fps, cin.parse::<u32>().unwrap());
+            println!("{}", result);
+        } else {
+            max_fps = max_fps_settings();
+        }
+    }
 }
