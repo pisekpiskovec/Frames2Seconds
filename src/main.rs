fa91e10d4109033d4384acf2192d8a981b3390a7
Písek Pískovec <77384152+pisekpiskovec@users.noreply.github.com>
2025-01-21 09:20:37 +0100

    Code Format

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
+use clap::Parser;
 use regex::Regex;
 use text_io::read;
-use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-struct Args{
+struct Args {
     #[arg(short, long, default_value_t = 0.0)]
     max_fps: f32,
 
@@ -21,7 +21,10 @@ fn max_fps_settings() -> f32 {
 }
 
 fn calculate_miliseconds(max_dot_seconds: u32, max_fps: f32, frame_of_second: u32) -> u32 {
-    if max_fps <= 0.0 { eprintln!("Error: Can't divide with 0!"); return 0;}
+    if max_fps <= 0.0 {
+        eprintln!("Error: Can't divide with 0!");
+        return 0;
+    }
     let tmp_a: u128 = max_dot_seconds as u128 * frame_of_second as u128;
     let tmp_b: f64 = (tmp_a as f64) / max_fps as f64;
     tmp_b as u32
@@ -32,10 +35,18 @@ fn main() {
     const MAX_DOT_SECONDS: u32 = 999999999;
 
     let regexp = Regex::new(r"[\d]+$").unwrap();
-    let mut max_fps: f32 = if args.max_fps != 0.0 {args.max_fps} else {max_fps_settings()};
+    let mut max_fps: f32 = if args.max_fps != 0.0 {
+        args.max_fps
+    } else {
+        max_fps_settings()
+    };
     loop {
         print!("Enter frame number (or letters to reconfigure max FPS, press C-c to quit): ");
-        let cin: String = if let Some(get_fps_arg) = args.fps.as_deref() {String::from(get_fps_arg)} else {read!()};
+        let cin: String = if let Some(get_fps_arg) = args.fps.as_deref() {
+            String::from(get_fps_arg)
+        } else {
+            read!()
+        };
 
         if regexp.is_match(&cin) {
             let result =
